6e5c44427406175679830641366a8f162a76245e
Czakero <bartosz.pyrz96@gmail.com>
2019-01-07 10:07:37 +0100

    Implement CSVFile class which holds rows to act as a table

---
+++ src/main/java/com/sqlcsv/sqlcsv/model/CSVFile.java
@@ -0,0 +1,17 @@
+package com.sqlcsv.sqlcsv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CSVFile {
+    private String name;
+    private List<Row> rows;
+}
