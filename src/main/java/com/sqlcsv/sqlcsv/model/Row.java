a84a72d373694c355c10beccacbb19fe3e8eebff
Czakero <bartosz.pyrz96@gmail.com>
2019-01-07 10:06:57 +0100

    Implement row class which holds data from csv

---
+++ src/main/java/com/sqlcsv/sqlcsv/model/Row.java
@@ -0,0 +1,16 @@
+package com.sqlcsv.sqlcsv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Row {
+    private List<String> data;
+}
