96930ad88d7f3e5e831f89b39beee0b47962298b
Pieczkowski <pieczkowski.t@gmail.com>
2019-01-09 15:11:33 +0100

    Fix indentations in SelectService

--- src/main/java/com/sqlcsv/sqlcsv/service/SelectService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/SelectService.java
@@ -12,63 +12,63 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SelectService {
-        private String fileName;
+    private String fileName;
 
-        public SelectService(String filename){
-            this.fileName = filename;
+    public SelectService(String filename){
+        this.fileName = filename;
+    }
+
+    public List<String> readHeaders(){
+        try(Stream<String> stream = Files.lines(Paths.get(fileName))){
+            return stream.limit(1).flatMap(s -> Arrays.stream(s.split(","))).collect(Collectors.toList());
+        } catch (IOException e){
+            e.printStackTrace();
         }
+        return new ArrayList<>();
+    }
 
-        public List<String> readHeaders(){
-            try(Stream<String> stream = Files.lines(Paths.get(fileName))){
-                return stream.limit(1).flatMap(s -> Arrays.stream(s.split(","))).collect(Collectors.toList());
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            return new ArrayList<>();
-        }
+    public List<String> selectWhere(String whereClause){
+        String wherePattern = "^(\\w+)\\s(\\w+)\\s(\\w+)\\s(.)?"; // column_name like wow
+        Pattern pattern = Pattern.compile(wherePattern);
+        Matcher matcher = pattern.matcher(whereClause);
 
-        public List<String> selectWhere(String whereClause){
-            String wherePattern = "^(\\w+)\\s(\\w+)\\s(\\w+)\\s(.)?"; // column_name like wow
-            Pattern pattern = Pattern.compile(wherePattern);
-            Matcher matcher = pattern.matcher(whereClause);
+        if (matcher.find()) {
+            String columnName = matcher.group(1);
+            String operation = matcher.group(2);
+            String condition = matcher.group(3);
 
-            if (matcher.find()) {
-                String columnName = matcher.group(1);
-                String operation = matcher.group(2);
-                String condition = matcher.group(3);
+            Integer index = getIndex(columnName);
 
-                Integer index = getIndex(columnName);
-
-                if (index != null) {
-                    try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-                        return filterStream(stream, index, operation, condition).collect(Collectors.toList());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            if (index != null) {
+                try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                    return filterStream(stream, index, operation, condition).collect(Collectors.toList());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            return null;
         }
+        return null;
+    }
 
-        private Integer getIndex(String columnName){
-            List<String> headers = readHeaders();
-            for (int i = 0; i < headers.size(); i++) {
-                if (headers.get(i).equals(columnName))
-                    return i;
-            }
-            return null;
+    private Integer getIndex(String columnName){
+        List<String> headers = readHeaders();
+        for (int i = 0; i < headers.size(); i++) {
+            if (headers.get(i).equals(columnName))
+                return i;
         }
+        return null;
+    }
 
-        private Stream<String> filterStream(Stream<String> stream, Integer index, String operation, String condition) {
-            return stream.skip(1).filter(s -> isConditionTrue(s.split(",")[index], operation, condition));
+    private Stream<String> filterStream(Stream<String> stream, Integer index, String operation, String condition) {
+        return stream.skip(1).filter(s -> isConditionTrue(s.split(",")[index], operation, condition));
+    }
+
+    private boolean isConditionTrue(String valueFromTable, String operation, String condition) {
+        boolean result = false;
+
+        if (operation.equals("like")) {
+            result = valueFromTable.equals(condition);
         }
-
-        private boolean isConditionTrue(String valueFromTable, String operation, String condition) {
-            boolean result = false;
-
-            if (operation.equals("like")) {
-                result = valueFromTable.equals(condition);
-            }
-            return result;
-        }
+        return result;
+    }
 }
