3c39a5027e47d1676549ef3ae2e79122c26ed2c5
Pieczkowski <pieczkowski.t@gmail.com>
2019-01-09 16:30:41 +0100

    Add testGettingCorrectHeadersFromFile

--- src/test/java/com/sqlcsv/sqlcsv/service/SelectServiceTest.java
+++ src/test/java/com/sqlcsv/sqlcsv/service/SelectServiceTest.java
@@ -1,6 +1,13 @@
 package com.sqlcsv.sqlcsv.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SelectServiceTest {
     private SelectService selectService;
@@ -9,6 +16,13 @@ class SelectServiceTest {
     @BeforeEach
     void buildUp(){
             selectService = new SelectService(FILEPATH);
+    }
+
+    @Test
+    void testGettingCorrectHeadersFromFile() {
+        List<String> expected = new ArrayList<> (Arrays.asList("first_name", "last_name", "age", "city"));
+        List<String> result = selectService.readHeaders();
+        assertEquals(expected, result);
     }
 
 }
