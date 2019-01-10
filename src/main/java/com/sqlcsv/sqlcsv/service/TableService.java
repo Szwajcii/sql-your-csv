75cf883a02c28e78a0c4ae475681b490d9edf238
Czakero <bartosz.pyrz96@gmail.com>
2019-01-10 13:51:22 +0100

    Basic skeleton of service

--- src/main/java/com/sqlcsv/sqlcsv/service/TableService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/TableService.java
@@ -1,32 +1,16 @@
 package com.sqlcsv.sqlcsv.service;
 
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-import com.sqlcsv.sqlcsv.google.GoogleAPI;
 import com.sqlcsv.sqlcsv.model.Table;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service
 public class TableService implements TableServiceInterface {
-    private GoogleAPI api;
 
-    @Autowired
-    public TableService(GoogleAPI api) {
-        this.api = api;
-    }
 
     @Override
-    public List<String> getAllSpreadsheetsIds() throws IOException {
-        FileList result = api.getDrive().files().list().execute();
-        List<File> filess = result.getFiles();
-        System.out.println("Files:");
-        for (File file : filess) {
-            System.out.printf("%s (%s)\n", file.getName(), file.getId());
-        }
+    public List<String> getAllSpreadsheetsIds() {
         return null;
     }
 
