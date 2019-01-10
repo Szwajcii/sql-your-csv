46d306cc63fa9bcab8bd85c4105dded528cfa4b2
Czakero <bartosz.pyrz96@gmail.com>
2019-01-10 13:51:44 +0100

    Basic controller checking authorization flow

--- src/main/java/com/sqlcsv/sqlcsv/controller/exception/OneToRuleThemAllController.java
+++ src/main/java/com/sqlcsv/sqlcsv/controller/exception/OneToRuleThemAllController.java
@@ -1,26 +1,45 @@
 package com.sqlcsv.sqlcsv.controller.exception;
 
-import com.sqlcsv.sqlcsv.service.TableServiceInterface;
+import com.sqlcsv.sqlcsv.google.GoogleAuthorizationFlow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class OneToRuleThemAllController {
-    private TableServiceInterface tsi;
+
+    private GoogleAuthorizationFlow googleAuthorizationFlow;
 
     @Autowired
-    public OneToRuleThemAllController(TableServiceInterface tsi) {
-        this.tsi = tsi;
+    public OneToRuleThemAllController(GoogleAuthorizationFlow googleAuthorizationFlow) {
+        this.googleAuthorizationFlow = googleAuthorizationFlow;
     }
 
     @GetMapping("/")
-    public String doGet(Model model) throws IOException {
-        tsi.getAllSpreadsheetsIds();
+    public void getAuth(HttpServletResponse response) {
+        String url = googleAuthorizationFlow.getFlow().newAuthorizationUrl()
+                .setRedirectUri("http://localhost:8080/callback")
+                .build();
+        try {
+            response.sendRedirect(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @GetMapping("/home")
+    public String doGet(Model model) {
         model.addAttribute("entry", "yeah it's working!");
+        return "home";
+    }
+
+    @GetMapping("/callback")
+    public String getToken(HttpServletRequest request) {
+        String code = request.getParameter("code");
+        System.out.println(code);
         return "home";
     }
 }
