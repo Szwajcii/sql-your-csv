5e8034493d958613b70ce3fff7f6e421d909a323
Czakero <bartosz.pyrz96@gmail.com>
2019-01-11 10:46:15 +0100

    Refactor for project needs

--- src/main/java/com/sqlcsv/sqlcsv/google/GoogleAuthorizationFlow.java
+++ src/main/java/com/sqlcsv/sqlcsv/google/GoogleAuthorizationFlow.java
@@ -6,7 +6,9 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -26,16 +29,11 @@ public class GoogleAuthorizationFlow {
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final java.io.File CREDENTIALS_FOLDER = new java.io.File("/home/bartosz/Codecool/Advanced/sql-your-csv/src/main/resources");
     private static final String CLIENT_SECRET_FILE_NAME = "client_secret.json";
-    private static final List<String> SCOPES = List.of(DriveScopes.DRIVE_READONLY, SheetsScopes.SPREADSHEETS_READONLY);
+    private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE_READONLY, SheetsScopes.SPREADSHEETS_READONLY);
+    private static GoogleAuthorizationCodeFlow flow;
 
-    private GoogleAuthorizationCodeFlow flow;
-
-    public GoogleAuthorizationFlow() throws IOException, GeneralSecurityException {
-        this.flow = getNewFlow();
-    }
-
-    private GoogleAuthorizationCodeFlow getNewFlow() throws IOException, GeneralSecurityException {
-        return new GoogleAuthorizationCodeFlow.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY,
+    public static GoogleAuthorizationCodeFlow getNewFlow() throws IOException, GeneralSecurityException {
+        return flow = new GoogleAuthorizationCodeFlow.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY,
                 getClientSecrets(), SCOPES).setDataStoreFactory(new FileDataStoreFactory(CREDENTIALS_FOLDER))
                 .setAccessType("offline").setApprovalPrompt("force").build();
     }
@@ -44,5 +42,16 @@ public class GoogleAuthorizationFlow {
         java.io.File clientSecretFile = new java.io.File(CREDENTIALS_FOLDER, CLIENT_SECRET_FILE_NAME);
         InputStream clientSecretInputStream = new FileInputStream(clientSecretFile);
         return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(clientSecretInputStream));
+    }
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, flow.loadCredential("user"))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static Drive getDriveService() throws IOException, GeneralSecurityException {
+        return new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, flow.loadCredential("user"))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 }
